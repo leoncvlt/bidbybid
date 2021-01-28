cdb29e6281c429f34e896eda2ffd5dd7b4b5c84e
Leonardo Cavaletti <leon@random42.com>
2021-01-28 17:05:29 +0000

    Added more args, average, rich progress / logs

--- bidbybid.py
+++ bidbybid.py
@@ -5,7 +5,9 @@ import os
 import sys
 import logging
 import argparse
+import locale
 from html import escape
+from statistics import mean
 from pathlib import Path
 
 from selenium import webdriver
@@ -18,32 +20,48 @@ from selenium.common.exceptions import NoSuchElementException
 
 import chromedriver_autoinstaller
 
+from rich.logging import RichHandler
+from rich.progress import Progress
+from rich.traceback import install as install_rich_tracebacks
+
 from dateutil.parser import parse as parse_date
 from price_parser import Price
 
+import pygal
 
+install_rich_tracebacks()
 log = logging.getLogger(__name__)
+progress = Progress(transient=True)
 
 
-def initialize_chromedriver():
-    chromedriver_autoinstaller.install()
+# TODO add more locales
+# TODO https://lastdropofink.co.uk/market-places/ebay/global-international-ebay-site-list/
+# TODO http://journals.ecs.soton.ac.uk/java/tutorial/intl/datamgmt/locales.html
+EBAY_DOMAINS = {
+    "en_US": "com",
+    "en_UK": "co.uk",
+}
 
 
-def scrape_search_term(search, domain="co.uk"):
-    url = f"https://www.ebay.{domain}/sch/i.html?_nkw={escape(search)}&LH_Sold=1&LH_Complete=1&LH_Auction=1&_ipg=200"
+def scrape_search_term(search, locale_str="en_UK"):
+    locale.setlocale(locale.LC_ALL, locale_str)
+    url = (
+        f"https://www.ebay.{EBAY_DOMAINS[locale_str]}/sch/i.html"
+        f"?_nkw={escape(search)}&LH_Sold=1&LH_Complete=1&LH_Auction=1&_ipg=200"
+    )
     return scrape_url(url)
 
 
-def scrape_url(url, driver=None, items=[]):
+def scrape_url(url, driver=None, items=[], progress_task=None, headless=True):
     if driver is None:
         chromedriver_path = chromedriver_autoinstaller.install()
-        log.info(f"Initialising chromedriver at {chromedriver_path}")
         logs_path = Path.cwd() / "logs" / "webdrive.log"
-
         logs_path.parent.mkdir(parents=True, exist_ok=True)
+
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("window-size=1920,1080")
+        if headless:
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("window-size=1920,1080")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--log-level=4")
@@ -57,44 +75,58 @@ def scrape_url(url, driver=None, items=[]):
         )
 
     driver.get(url)
+    with progress:
+        if progress_task is None:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "srp-controls__count-heading")
+                )
+            )
+            str_total = driver.find_elements_by_css_selector(
+                ".srp-controls__count-heading > span"
+            )[0].text
+            total = locale.atoi(str_total)
+            log.info(f"Found {total} sold auctions")
+            progress_task = progress.add_task("Scraping auctions...", total=total)
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "srp-results"))
-    )
-    results = driver.find_elements_by_css_selector(".srp-results li.s-item")
-    for result in results:
-        title = result.find_element_by_css_selector(".s-item__title")
-        date = result.find_element_by_css_selector(".s-item__title--tagblock .POSITIVE")
-        price = result.find_element_by_css_selector(".s-item__price > span")
-        link = result.find_element_by_css_selector("a.s-item__link")
-
-        items.append(
-            {
-                "title": title.text,
-                "date": parse_date(date.text.split("Sold")[1]),
-                "price": Price.fromstring(price.text).amount_float,
-                "url": link.get_attribute("href"),
-            }
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "srp-results"))
         )
+        results = driver.find_elements_by_css_selector(".srp-results li.s-item")
+        for result in results:
+            title = result.find_element_by_css_selector(".s-item__title")
+            date = result.find_element_by_css_selector(
+                ".s-item__title--tagblock .POSITIVE"
+            )
+            price = result.find_element_by_css_selector(".s-item__price > span")
+            link = result.find_element_by_css_selector("a.s-item__link")
 
-    try:
-        next_button = driver.find_element_by_css_selector("a.pagination__next")
-        next_url = next_button.get_attribute("href")
-        has_more = driver.current_url != next_url
-        if has_more:
-            return scrape_url(next_button.get_attribute("href"), driver, items)
-    except NoSuchElementException:
-        log.debug("Reached end of results")
-        pass
+            progress.update(progress_task, advance=1)
+            items.append(
+                {
+                    "title": title.text,
+                    "date": parse_date(date.text.split("Sold")[1]),
+                    "price": Price.fromstring(price.text).amount_float,
+                    "url": link.get_attribute("href"),
+                }
+            )
 
-    return items
-    driver.quit()
+        try:
+            next_button = driver.find_element_by_css_selector("a.pagination__next")
+            next_url = next_button.get_attribute("href")
+            has_more = driver.current_url != next_url
+            if has_more:
+                return scrape_url(next_url, driver, items, progress_task)
+        except NoSuchElementException:
+            # reached end of results
+            pass
 
-
-import pygal
+        driver.quit()
+        return items
 
 
 def chart_scraped_data(data):
+    multiple = len(data) > 1
     chart = pygal.DateTimeLine(
         show_legend=True,
         width=960,
@@ -102,16 +134,29 @@ def chart_scraped_data(data):
         truncate_label=-1,
         x_value_formatter=lambda dt: dt.strftime("%b %Y"),
     )
-    for search, scraped_items in data.items():
+    for result in data:
         chart_items = [
             {
                 "value": (item["date"], item["price"]),
                 "label": item["title"],
                 "xlink": item["url"],
             }
-            for item in scraped_items
+            for item in result["items"]
         ]
-        chart.add(search, chart_items)
+        series_name = result["search"] if multiple else "auctions"
+        chart.add("auctions", chart_items)
+
+        # if we are rendering a single search, add the average and trend line
+        if not multiple:
+            chart.add(
+                "average",
+                [
+                    {"value": (result["start"], result["average"])},
+                    {"value": (result["end"], result["average"])},
+                ],
+            )
+
+    # render the chart
     chart.render_in_browser()
 
 
@@ -123,66 +168,76 @@ def main():
         help="The ebay search terms",
     )
     argparser.add_argument(
+        "--locale",
+        choices=list(EBAY_DOMAINS.keys()),
+        default="en_US",
+        help="The locale to run the search in"
+        " - will set the eBay's country domain and currency / dates parsing.",
+    )
+    argparser.add_argument(
+        "--exclude-anomalies",
+        action="store_true",
+        help="Excludes auctions which strays ",
+    )
+    argparser.add_argument(
+        "--anomalies-bias",
+        type=float,
+        default=0.5,
+        help="Bias for excluding anomalies"
+        "(e.g. a bias of 0.25 will exclude any auctions which sold at 25%% less or more than the average sold price)."
+        "Only applicaple with --exclude-anomalies. Default is 0.5",
+    )
+    argparser.add_argument(
+        "-c", "--chart", action="store_true", help="Displays the scraped results in chart"
+    )
+    argparser.add_argument(
         "-v", "--verbose", action="store_true", help="Increase output log verbosity"
     )
     args = argparser.parse_args()
 
     # configure logging for the application
-    log = logging.getLogger()
     log.setLevel(logging.INFO if not args.verbose else logging.DEBUG)
-    log_screen_handler = logging.StreamHandler(stream=sys.stdout)
-    log_screen_handler.setFormatter(
-        logging.Formatter(
-            fmt="%(asctime)s %(levelname)-8s %(message)s", datefmt="[%H:%M:%S]"
-        )
-    )
-    log.addHandler(log_screen_handler)
+    rich_handler = RichHandler()
+    rich_handler.setFormatter(logging.Formatter(fmt="%(message)s", datefmt="[%X]"))
+    log.addHandler(rich_handler)
     log.propagate = False
-
-    # if colorama is present, add some color to the logs
-    try:
-        from colorama import Style, Fore, Back
-        import copy
-
-        LOG_COLORS = {
-            logging.DEBUG: Fore.GREEN,
-            logging.INFO: Fore.BLUE,
-            logging.WARNING: Fore.YELLOW,
-            logging.ERROR: Fore.RED,
-            logging.CRITICAL: Back.RED,
-        }
-
-        class ColorFormatter(logging.Formatter):
-            def format(self, record, *args, **kwargs):
-                # if the corresponding logger has children, they may receive modified
-                # record, so we want to keep it intact
-                new_record = copy.copy(record)
-                if new_record.levelno in LOG_COLORS:
-                    new_record.levelname = "{color_begin}{level}{color_end}".format(
-                        level=new_record.levelname,
-                        color_begin=LOG_COLORS[new_record.levelno],
-                        color_end=Style.RESET_ALL,
-                    )
-                return super(ColorFormatter, self).format(new_record, *args, **kwargs)
-
-        max_log_w = len(f"{LOG_COLORS[logging.CRITICAL]}CRITICAL{Style.RESET_ALL}")
-        log_screen_handler.setFormatter(
-            ColorFormatter(
-                fmt=f"%(asctime)s %(levelname)-{max_log_w}s %(message)s",
-                datefmt="{color_begin}[%H:%M:%S]{color_end}".format(
-                    color_begin=Style.DIM, color_end=Style.RESET_ALL
-                ),
-            )
-        )
-    except ModuleNotFoundError as identifier:
-        pass
 
     # start the application
     log.debug(f"Starting application with args {vars(args)}")
-    initialize_chromedriver()
-    data = {}
+    data = []
     for search in args.search.split(","):
-        data[search] = scrape_search_term(search)
+        items = scrape_search_term(search, locale_str=args.locale)
+        average = mean(item["price"] for item in items)
+
+        # if the exclude_anomalies flag is turned on, calculate the variation from the
+        # anomalies_bias and filer out all auctions whose price falls outside it
+        if args.exclude_anomalies:
+            variation = average * args.anomalies_bias
+            floor = average - variation
+            ceiling = average + variation
+            items = [item for item in items if floor <= item["price"] <= ceiling]
+            # recalculate the average once done
+            average = mean(item["price"] for item in items)
+
+        # calculate start and end range for the search
+        dates = [item["date"] for item in items]
+        start = min(dates)
+        end = max(dates)
+
+        # add search result to the dataset
+        data.append(
+            {
+                "search": search,
+                "start": start,
+                "end": end,
+                "items": items,
+                "average": average,
+            }
+        )
+
+    if not data:
+        sys.exit(0)
+
     chart_scraped_data(data)
 
 
